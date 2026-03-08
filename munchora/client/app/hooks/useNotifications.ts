186267ed765f7dca57cf089563b05d6e4f3626c6
realkoder <alexanderbtcc@gmail.com>
2026-03-08 23:01:51 +0100

    update url to match notifications-service with minikube

--- munchora/client/app/hooks/useNotifications.ts
+++ munchora/client/app/hooks/useNotifications.ts
@@ -5,8 +5,9 @@ import useRecipeEvents from "~/hooks/useRecipeEvents";
 
 const rawBaseUrl = import.meta.env.VITE_BASE_URL || 'localhost:3000';
 const baseUrl = rawBaseUrl.replace(/^https?:\/\//, '');
+const notificationsServiceBaseUrl = `ws://${baseUrl}/notifications${baseUrl === 'localhost' ? '-app' : ''}/cable`;
 
-const WEBSOCKET_URL = process.env.NODE_ENV === 'development' ? `ws://${baseUrl}/notifications/cable` : 'wss://munchora.pro/notifications/cable';
+const WEBSOCKET_URL = process.env.NODE_ENV === 'development' ? notificationsServiceBaseUrl : 'wss://munchora.pro/notifications/cable';
 
 export enum NotificationType {
   RECIPE_CREATED = 'recipe_created',
